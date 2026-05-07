b3bf376316236302419d2a91803aa1b71da67a76
xaviermarcio <xaviermarcio80@gmail.com>
2026-05-07 10:06:35 -0300

    Atualição usuário

--- public/js/data.js
+++ public/js/data.js
@@ -5,7 +5,7 @@ export const CONFIG_SISTEMA = {
     ],
     lojas: [
         { id: "entre_lagos", nome: "Entre Lagos", operadores: ["Lindomar", "Márcio Xavier"] },
-        { id: "itapoa_parque", nome: "Itapoã Parque", operadores: ["Amanda", "Italo", "Jemerson", "Ygor"] }
+        { id: "itapoa_parque", nome: "Itapoã Parque", operadores: ["Amanda", "Italo", "Ygor"] }
     ],
     produtos: [
         { cod: "2073", nome: "ABACATE", unidade: "KG" },
